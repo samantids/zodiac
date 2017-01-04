c7ad1d242371f2d79b4bc9e2c94979bc2d0c99e2
Samantha Schwartz <samanthadschwartz@gmail.com>
2017-01-03 21:17:57 -0500

    add styling, position match hover dates

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ var SUBMIT = document.getElementById("submit"),
 
 var SIGN_LIST = [{"sign": "Capricorn", "element": "Earth", "color": "Brown", "day": "Saturday", "ruler": "Saturn", "matches": ["Cancer", "Taurus", "Virgo"], "dates": ["12/22", "1/19"]},
 				{"sign": "Aquarius", "element": "Air", "color": "Blue", "day": "Sunday", "ruler": "Uranus", "matches": ["Leo", "Gemini", "Libra"], "dates": ["1/20", "2/18"]},
-				{"sign": "Pisces", "element": "Water", "color": "Lilac", "day": "Thursday", "ruler": "Neptune", "matches": ["Virgo", "Cancer", "Scorpio"], "dates": ["2/19", "3/20"]},
+				{"sign": "Pisces", "element": "Water", "color": "Purple", "day": "Thursday", "ruler": "Neptune", "matches": ["Virgo", "Cancer", "Scorpio"], "dates": ["2/19", "3/20"]},
 				{"sign": "Aries", "element": "Fire", "color": "Red", "day": "Tuesday", "ruler": "Mars", "matches": ["Libra", "Leo", "Sagitarius"], "dates": ["3/21", "4/19"]},
 				{"sign": "Taurus", "element": "Earth", "color": "Pink", "day": "Friday", "ruler": "Venus", "matches": ["Scorpio", "Virgo", "Capricorn"], "dates": ["4/20", "5/20"]},
 				{"sign": "Gemini", "element": "Air", "color": "Yellow", "day": "Wednesday", "ruler": "Mercury", "matches": ["Sagitarius", "Libra", "Aquarius"], "dates": ["5/21", "6/20"]},
 				{"sign": "Cancer", "element": "Water", "color": "Orange", "day": "Monday", "ruler": "Moon", "matches": ["Capricorn", "Scorpio", "Pisces"], "dates": ["6/21", "7/22"]},
 				{"sign": "Leo", "element": "Fire", "color": "Gold", "day": "Sunday", "ruler": "Sun", "matches": ["Aquarius", "Aries", "Sagitarius"], "dates": ["7/23", "8/22"]},
-				{"sign": "Virgo", "element": "Earth", "color": "Beige", "day": "Wednesday", "ruler": "Mercury", "matches": ["Pisces", "Taurus", "Capricorn"], "dates": ["8/23", "9/22"]},
+				{"sign": "Virgo", "element": "Earth", "color": "Green", "day": "Wednesday", "ruler": "Mercury", "matches": ["Pisces", "Taurus", "Capricorn"], "dates": ["8/23", "9/22"]},
 				{"sign": "Libra", "element": "Air", "color": "Green", "day": "Friday", "ruler": "Venus", "matches": ["Aries", "Gemini", "Aquarius"], "dates": ["9/23", "10/22"]},
-				{"sign": "Scorpio", "element": "Water", "color": "Rust", "day": "Tuesday", "ruler": "Pluto", "matches": ["Taurus", "Cancer", "Pisces"], "dates": ["10/23", "11/21"]},
+				{"sign": "Scorpio", "element": "Water", "color": "Red", "day": "Tuesday", "ruler": "Pluto", "matches": ["Taurus", "Cancer", "Pisces"], "dates": ["10/23", "11/21"]},
 				{"sign": "Sagitarius", "element": "Fire", "color": "Violet", "day": "Thursday", "ruler": "Jupiter", "matches": ["Gemini", "Aries", "Leo"], "dates": ["11/22", "12/21"]}],
 	SIGN_ELEMENT = "",
 	SIGN_COLOR = "",
@@ -51,7 +51,6 @@ function getDates(matchsign){
 
 
 function signPick(){
-	console.log("signPick initiated");
 	//clear previous content from your-sign div
 	YOUR_SIGN.innerHTML = "";
 
@@ -106,11 +105,10 @@ function signPick(){
 
 	if (SIGN === "" || MONTH_SELECTED.value == "" || DAY_SELECTED.value == ""){
 		YOUR_SIGN.innerHTML += "<h2>Error: That date does not exist. Try again.</h2>";
-		console.log("Error: That date does not exist");
 	} else {
 		YOUR_SIGN.innerHTML += "<h2>Your birthday is " + MONTH_PLUS_ONE + "/" + DAY_SELECTED.value + 
 					" and your sign is " + SIGN + "!</h2>" + 
-					"<br>" + SIGN + " is one of the ~" + SIGN_ELEMENT + "~ signs." +
+					/*"<br>" +*/ SIGN + " is one of the ~" + SIGN_ELEMENT + "~ signs." +
 					"<br>Your power color is " + "~<span style='color:" + SIGN_COLOR + "'>" + SIGN_COLOR + "</span>~." + 
 					"<br>Your best day of the week is usually a " + "~" + SIGN_DAY + "~." + 
 					"<br>Look to the sky: ~" + SIGN_RULER + "~ is your ruler!" + 
@@ -125,18 +123,18 @@ function matchHover(){
 	var MATCH_TEXT = document.getElementsByClassName("match-text"),
 		MATCH_POPUP = document.getElementById("popup");	
 
-	console.log(MATCH_TEXT);
-
 	for (var m of MATCH_TEXT){
 		m.onmouseover = function(e){
-			console.log(e);
 			var matchText = e.target.innerHTML;
 			var matchDates = getDates(matchText);
+			var popUpPositionX = e.target.offsetLeft - 10;
+			var popUpPositionY = e.target.offsetTop + 25;
 			MATCH_POPUP.innerHTML += matchDates[0] + " - " + matchDates[1];
 			MATCH_POPUP.style.display = "block"; 
-			/*MATCH_POPUP.style.position = "absolute";
-			MATCH_POPUP.style.left = e.x;
-			MATCH_POPUP.style.top = e.y; */
+			MATCH_POPUP.style.position = "absolute";
+			MATCH_POPUP.style.left = popUpPositionX + "px";
+			MATCH_POPUP.style.top = popUpPositionY + "px"; 
+			e.target.style.backgroundColor = "grey";
 		}
 	}
 
@@ -144,19 +142,18 @@ function matchHover(){
 		m.onmouseout = function(e){
 			MATCH_POPUP.innerHTML = "";
 			MATCH_POPUP.style.display = "none";
+			e.target.style.backgroundColor = "white";
 		}
 	}
 
 }
 
 function init(){
-	console.log("init start");
 	signPick();
 	matchHover();
 }
 
 
-//document.getElementById("submit").onclick = init();
 document.addEventListener("DOMContentLoaded", function(){
 	document.getElementById("submit").addEventListener("click", function(){
 		init();
